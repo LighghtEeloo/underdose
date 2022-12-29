8c895ec3e07af57813bd0e142b36e41e720f08e7
LighghtEeloo <litiaeeloo@gmail.com>
2022-12-29 02:08:09 -0500

    Incr.

--- src/bin/underdose.rs
+++ src/bin/underdose.rs
@@ -1,5 +1,78 @@
-use std::path::PathBuf;
+#![allow(unused)]
+
+use git2::Repository;
+use std::{
+    collections::{HashMap, HashSet},
+    path::PathBuf,
+};
+use underdose::{Atom, AtomTaskMode::*, Drip, DrugStore, Pill};
 
 fn main() {
-    println!("Hello seikai.")
+    env_logger::init();
+
+    let home_path = std::env::var("HOME").unwrap();
+    let drugstore_path = PathBuf::from(&home_path).join(format!(".IronCloak/"));
+    let store = DrugStore {
+        env: HashMap::new(),
+        pill: vec![Pill {
+            name: format!("nvim"),
+            drip: vec![Drip {
+                env: HashSet::new(),
+                root: (&home_path).into(),
+                pill: PathBuf::from(&home_path).join(format!(".IronCloak/Zshrc/")),
+                stem: vec![
+                    Atom {
+                        site: format!(".zshrcx/").into(),
+                        repo: format!(".zshrcx/").into(),
+                        mode: FileCopy,
+                    },
+                    Atom {
+                        site: format!(".zshrc").into(),
+                        repo: format!(".zshrc").into(),
+                        mode: FileCopy,
+                    },
+                ],
+            }],
+        }],
+        tutorial: None,
+    };
+
+    for pill in store.pill {
+        for drip in &pill.drip {
+            log::info!(
+                "\n[[{}]]::resolve_atoms_to_repo: {:#?}",
+                pill.name,
+                drip.resolve_atoms_to_repo()
+                    .into_iter()
+                    .map(|task| {
+                        format!(
+                            "<{}> {} {} {}",
+                            task.mode,
+                            task.src.display(),
+                            task.mode.display_arrow(),
+                            task.dst.display()
+                        )
+                    })
+                    .collect::<Vec<String>>()
+            )
+        }
+        // for drip in &pill.drip {
+        //     log::info!(
+        //         "\n[[{}]]::resolve_atoms_to_site: {:#?}",
+        //         pill.name,
+        //         drip.resolve_atoms_to_site()
+        //             .into_iter()
+        //             .map(|task| {
+        //                 format!(
+        //                     "<{}> {} {} {}",
+        //                     task.mode,
+        //                     task.src.display(),
+        //                     task.mode.display_arrow(),
+        //                     task.dst.display()
+        //                 )
+        //             })
+        //             .collect::<Vec<String>>()
+        //     )
+        // }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,130 @@
+#![allow(unused)]
+
 use serde::{Deserialize, Serialize};
+use std::fmt::Display;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
 use std::{
     collections::{HashMap, HashSet},
     path::PathBuf,
 };
+use toml_edit::Document;
 
+#[derive(Serialize, Deserialize)]
 pub struct DrugStore {
-    env: HashMap<String, HashSet<String>>,
+    /// a map of name -> upward dependencies, up to the root
+    pub env: HashMap<String, HashSet<String>>,
+    pub pill: Vec<Pill>,
+    pub tutorial: Option<()>,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct Pill {
-    name: String,
+    pub name: String,
+    pub drip: Vec<Drip>,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct Drip {
-    root: PathBuf,
-    env: Vec<String>,
-    stem: Vec<Atom>,
+    /// env is resolved to trivial form during parsing
+    pub env: HashSet<String>,
+    /// site root in the machine
+    pub root: PathBuf,
+    /// drip root in the drugstore
+    pub pill: PathBuf,
+    /// Atoms are incremented from drips but dirs aren't expanded
+    pub stem: Vec<Atom>,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct Atom {
-    site: PathBuf,
-    repo: PathBuf,
+    pub site: PathBuf,
+    pub repo: PathBuf,
+    pub mode: AtomTaskMode,
+}
+
+pub struct AtomTask {
+    pub src: PathBuf,
+    pub dst: PathBuf,
+    pub mode: AtomTaskMode,
+}
+
+#[derive(Serialize, Deserialize, Clone, Copy)]
+pub enum AtomTaskMode {
+    #[serde(rename = "copy")]
+    FileCopy,
+    #[serde(rename = "link")]
+    Link,
+}
+
+impl AtomTaskMode {
+    pub fn display_arrow(&self) -> &'static str {
+        match self {
+            Self::FileCopy => "==>",
+            Self::Link => "~~>",
+        }
+    }
+}
+
+impl Display for AtomTaskMode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            AtomTaskMode::FileCopy => write!(f, "copy"),
+            AtomTaskMode::Link => write!(f, "link"),
+        }
+    }
+}
+
+// impl DrugStore {
+//     pub fn of_file(mut file: File) -> Self {
+//         let mut content = String::new();
+//         file.read_to_string(&mut content);
+//         let doc = content.parse::<Document>().expect("invalid doc");
+//         let item = doc["env"].clone();
+//         todo!()
+//     }
+// }
+
+impl Drip {
+    pub fn check_env(&self, machine_env: HashSet<String>) -> bool {
+        true
+    }
+    /// Resolve stem atoms to absolute file paths; requires a direction
+    pub fn resolve_atoms<VisSrc, VisDst>(&self, src: &VisSrc, dst: &VisDst) -> Vec<AtomTask>
+    where
+        VisSrc: Fn(&Atom) -> &Path + Clone,
+        VisDst: Fn(&Atom) -> &Path + Clone,
+    {
+        fn extend_atom(tasks: &mut Vec<AtomTask>, src: &Path, dst: &Path, mode: AtomTaskMode) {
+            if src.is_file() || matches!(mode, AtomTaskMode::Link) {
+                tasks.push(AtomTask {
+                    src: src.to_owned(),
+                    dst: dst.to_owned(),
+                    mode,
+                })
+            } else if src.is_dir() {
+                for entry in src.read_dir().expect("read_dir failed") {
+                    let entry = entry.expect("entry failed");
+                    let path = entry.path();
+                    let file_name = path.file_name().expect("file_name failed");
+                    let dst_path = dst.join(file_name);
+                    extend_atom(tasks, &path, &dst_path, mode)
+                }
+            }
+        }
+        let mut tasks = Vec::new();
+        for atom in &self.stem {
+            let src_path = self.root.join(src(atom));
+            let dst_path = self.pill.join(dst(atom));
+            extend_atom(&mut tasks, &src_path, &dst_path, atom.mode)
+        }
+        tasks
+    }
+    pub fn resolve_atoms_to_repo(&self) -> Vec<AtomTask> {
+        self.resolve_atoms(&|atom| atom.site.as_path(), &|atom| atom.repo.as_path())
+    }
+    pub fn resolve_atoms_to_site(&self) -> Vec<AtomTask> {
+        self.resolve_atoms(&|atom| atom.repo.as_path(), &|atom| atom.site.as_path())
+    }
 }
