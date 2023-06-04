c799d3b2eee6ca58f23026ff400e276224424314
LighghtEeloo <litiaeeloo@gmail.com>
2023-06-04 17:14:35 +0800

    Chore. Clean up.

--- src/cli/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod app;
-pub mod interface;
-pub mod proc;
-
-pub use interface::Cli;
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 #![allow(unused)]
 
-pub mod cli;
+pub mod cli {
+    pub mod app;
+    pub mod interface;
+    pub mod proc;
+
+    pub use interface::Cli;
+}
 pub mod dynamics;
 pub mod machine;
 pub mod store;
